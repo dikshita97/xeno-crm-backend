d38a59b5eb3ead30d06f7bca9fb0699447ba829d
dikshita97 <131396350+dikshita97@users.noreply.github.com>
2025-06-03 13:07:46 +0530

    passportGoogle.ts

--- src/utils/passportGoogle.ts
+++ src/utils/passportGoogle.ts
@@ -1,1 +1,42 @@
 // Passport strategy
+import passport from 'passport';
+import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
+import { User } from '../models/User';
+
+export const initGooglePassport = () => {
+  passport.use(
+    new GoogleStrategy(
+      {
+        clientID: process.env.GOOGLE_CLIENT_ID!,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+        callbackURL: '/auth/google/callback'
+      },
+      async (_, __, profile, done) => {
+        try {
+          let user = await User.findOne({ googleId: profile.id });
+          if (!user) {
+            user = await User.create({
+              googleId: profile.id,
+              displayName: profile.displayName,
+              email: profile.emails?.[0].value || ''
+            });
+          }
+          done(null, user);
+        } catch (err) {
+          done(err as any, undefined);
+        }
+      }
+    )
+  );
+
+  passport.serializeUser((user: any, done) => done(null, user.id));
+  passport.deserializeUser(async (id: string, done) => {
+    const user = await User.findById(id);
+    done(null, user);
+  });
+};
+
+export const ensureAuth = (req: any, res: any, next: any) => {
+  if (req.isAuthenticated()) return next();
+  res.status(401).json({ message: 'Unauthorized' });
+};
