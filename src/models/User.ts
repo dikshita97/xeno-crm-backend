e52b4080ccee4d893dc604eb8950f373fdd8bd9c
dikshita97 <131396350+dikshita97@users.noreply.github.com>
2025-06-03 13:04:35 +0530

    User.ts

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,1 +1,13 @@
 // User model
+import mongoose, { Document, Schema } from 'mongoose';
+export interface IUser extends Document {
+  googleId: string;
+  displayName: string;
+  email: string;
+}
+const userSchema = new Schema<IUser>({
+  googleId: { type: String, required: true, unique: true },
+  displayName: { type: String, required: true },
+  email: { type: String, required: true }
+});
+export const User = mongoose.model<IUser>('User', userSchema);
